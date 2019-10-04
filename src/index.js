54cb7763dad03d004590dc6c53b3619e8269364d
dsstewa <davidstewart@gmail.com>
2019-10-03 21:54:42 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+
+//document.querySelector("#create-task-form").addEventListener("submit",function(event))
+
+
+document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    let item = document.querySelector("#new-task-description").value
+    document.getElementById("tasks").innerHTML += `<li>${item}</li>`;
+    event.preventDefault();
+
+}, false);
+
+
+
+
+
 });
